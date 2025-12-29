32f5a272f2e3ebd4b607a218c1341e52fe048e62
AsharMeraj <asharmeraj55@gmail.com>
2025-12-29 16:18:03 +0500

    Fixing UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import GHQPage from "./ghq/_components/GHQPage";
-import Navbar from "./_components/Navbar";
 import DemographicForm from "./_components/Demographic";
 
 export default function Home() {
